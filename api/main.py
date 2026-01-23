a0a7c17595726f3d70ce0ba7d4682e1e7cbd3d65
Raúl <raul.parada040103@gmail.com>
2026-01-23 13:06:17 +0100

    Añadido /hello en main.py + varias dependencias

---
+++ api/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, Request
+import os, requests
+
+app = FastAPI(title="ArgosBot")
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+URL_TELEGRAM = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}"
+
+@app.post("/webhook/telegram")
+async def telegram_webhook(request: Request):
+    data = await request.json()
+
+    try:
+        chat_id = data["message"]["chat"]["id"]
+        texto_recibido = data["message"].get("text", "")
+
+        # Si el usuario escribe /hello, ArgosBot responde
+        if texto_recibido == "/hello":
+            payload = {
+                "chat_id": chat_id,
+                "text": "¡Hola! Soy ArgosBot. El servidor funciona."
+            }
+            requests.post(URL_TELEGRAM, json=payload)
+
+    except KeyError:
+        pass
+
+    return {"status": "ok"}
+
+@app.get("/")
+async def home():
+    return "ArgosBot está activo"
